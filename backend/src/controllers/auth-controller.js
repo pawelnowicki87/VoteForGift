0fc0a5e40ecb057360452b64cd3879fc0e2efd5e
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2025-04-21 18:19:33 +0200

    added endpoint register

---
+++ backend/src/controllers/auth-controller.js
@@ -0,0 +1,10 @@
+
+
+const register = (req, res, next) => {
+res.send('register controller')
+}
+
+
+export const authController = {
+    register
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
+import 'dotenv/config';
 import express from 'express';
+import { authRouter } from './routers/auth-router.js';
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(authRouter);
 
 app.get('/', (req, res) => {
     res.send("Hello from index.js")
